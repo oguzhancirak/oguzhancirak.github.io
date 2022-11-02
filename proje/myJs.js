e94058cc31bcb1b1a66c26ee15f044d7aa64ac52
oğuzhan <40310832+ceyhan0@users.noreply.github.com>
2022-11-02 16:24:03 +0300

    Add files via upload

---
+++ proje/myJs.js
@@ -0,0 +1,22 @@
+// axios('https://gist.github.com/ozdemirburak/4821a26db048cc0972c1beee48a408de#file-cities_of_turkey-json', {
+//     method: 'GET'
+// }).then(response => ekranaGoster(response)).catch(hata=>console.log(hata));
+
+// const sehirleriCekme = document.getElementById('sehir').addEventListener('click', (e)=>{
+//     let sehirler = '';
+//     data.forEach(sehir => {
+//         sehirler += sehir;
+
+
+//     });
+//     sehirleriCekme.innerHTML = sehirler;
+// });
+
+
+
+
+
+
+
+
+
